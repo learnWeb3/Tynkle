7ef6b3cb30edc29e949983b4e6e0ef3159ef5f86
antoine-tech <antoinehadrienleguillou@outlook.com>
2021-01-12 15:27:24 +0100

    adding activities page containing askings and offers

---
+++ activity.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon compte</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="./assets/css/mdb.min.css">
+    <link rel="stylesheet" href="./assets/css/commons/main.css">
+    <link rel="stylesheet" href="./assets/css/pages/activity.css">
+</head>
+
+<body>
+
+    <?php include_once './commons/navbar.php' ?>
+
+
+    <main class="bg-white" style='min-height:100vh;margin-top:5rem'>
+        <section class="container d-flex flex-column align-items-center">
+            <div class="row col-12 justify-content-center">
+                <button class="btn btn-lg btn-brand col-12 col-lg-3 m-2 active">Mes annonces</button>
+                <button class="btn btn-lg btn-brand col-12 col-lg-3 m-2">Mes demandes</button>
+            </div>
+            <div class="d-flex w-100 align-self-start px-2 py-4">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                    <label class="form-check-label" for="flexCheckDefault">
+                        En ligne
+                    </label>
+                </div>
+                <div class="form-check" style='margin-left:2rem;'>
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                       Archivées
+                    </label>
+                </div>
+            </div>
+            <div class='col-12'>
+                <?php for ($count = 0; $count < 100; $count++) : ?>
+
+                    <?php include './assets/partials/publication_card/publication_card.php' ?>
+
+                <?php endfor; ?>
+            </div>
+        </section>
+    </main>
+
+</body>
+
+</html>
---
+++ assets/partials/publication_card/publication_card.php
@@ -0,0 +1,28 @@
+<div class="card card-publication shadow-sm p-4 my-4">
+    <div class="row">
+        <div class="col-8">
+            <h5>Ordinateur bloqué, écran bl...</h5>
+            <p>Informatique - Dépanner mon PC/Mac</p>
+        </div>
+        <div class="col-4 d-flex align-items-center justify-content-end">
+            <img src="./assets/partials/publication_card/img/pointer.svg" height="16" width="16" alt="map pointer icon" class="img-fluid">
+            <p class="m-2 mb-0">Marseille (13012)</p>
+        </div>
+    </div>
+    <div class="row"><a href="">Voir plus....</a></div>
+    <div class="row">
+        <div class="col-8">
+            <p>John Malkovich</p>
+        </div>
+        <div class="col-4 d-flex flex-column justify-content-center align-items-end">
+            <div class="d-flex align-items-center justify-content-end w-100">
+                <img src="./assets/partials/publication_card/img/trash.svg" height="16" width="16" alt="delete publication" class="m-2 img-fluid">
+                <img src="./assets/partials/publication_card/img/edit.svg" height="16" width="16" alt="edit publication" class="m-2 img-fluid">
+            </div>
+            <div class="d-flex align-items-center justify-content-end w-100">
+                <img src="./assets/partials/publication_card/img/clock.svg" height="16" width="16" alt="edit publication" class="img-fluid mr-2">
+                <p class="m-2 mb-0">Posté le 01.01.2021</p>
+            </div>
+        </div>
+    </div>
+</div>
