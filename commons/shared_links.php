7101e1ec3a14a51a367a17f5af0f4195ee82bef9
antoine-tech <antoinehadrienlegullou@outlook.com>
2021-01-20 11:26:28 +0100

    removing templating HTML files

--- commons/shared_links.php
+++
@@ -1,7 +0,0 @@
-<ul class="d-flex flex-column list-unstyled" style='list-style-type:none'>
-    <li><small><a href="/signin">Se connecter</a></small>
-    </li>
-    <li><small><a href="/signin">S'inscrire</a></small>
-    </li>
-    <li><small><a href="/faq">F.A.Q</a></small></li>
-</ul>
