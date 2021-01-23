8fcc84e699a6a6f8ae07fa3e779bb4f0c4ee719c
antoine-tech <antoinehadrienlegullou@outlook.com>
2021-01-23 10:41:20 +0100

    adding helper class flash dealing with flash messages info displayed after main actions to user to inform him

--- app/controllers/ActivitiesController.php
+++ app/controllers/ActivitiesController.php
@@ -6,7 +6,6 @@ class ActivitiesController extends ApplicationController
     public function __construct(array $params, string $asked_method)
     {
         parent::__construct($params, $asked_method);
-        $this->isUserLoggedIn(['destroy', 'update']);
     }
     public function create()
     {
