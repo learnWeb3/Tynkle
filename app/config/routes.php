38e0e8a9498ecc503d4b5e09a1467cf3ebc2c351
antoine-tech <antoinehadrienlegullou@outlook.com>
2021-02-12 12:15:27 +0100

    reviwing posts#show js / new message and new offer send fron page show

--- app/config/routes.php
+++ app/config/routes.php
@@ -24,7 +24,7 @@ $routes = array(
     // /chats
     array('GET', ROOT_PATH . '/chats', 'chats#index', 'index_chat'),
     array('GET', ROOT_PATH . '/chats/[:chat_id]', 'chats#show', 'show_chat'),
-    array('POST', ROOT_PATH . '/chats/[:chat_id]', 'chats#create', 'create_message'),
+    array('POST', ROOT_PATH . '/chats', 'chats#create', 'create_message'),
     array('GET', ROOT_PATH . '/chats/[:chat_id]/stream', 'chats#stream', 'stream_message'),
     array('DELETE', ROOT_PATH . '/chats/[:chat_id]', 'chats#delete', 'delete_chat'),
     array('DELETE', ROOT_PATH . '/messages/[:message_id]', 'chats#delete', 'delete_message'),
