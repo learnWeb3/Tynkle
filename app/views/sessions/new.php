5cd5862a73b8b8dc2acb8272130c75e46770fb1e
antoine-tech <antoinehadrienlegullou@outlook.com>
2021-02-16 14:57:28 +0100

    mispelling correction

--- app/views/sessions/new.php
+++ app/views/sessions/new.php
@@ -2,11 +2,11 @@
 
     <section class="col-12 col-xl-4 offset-xl-1 d-flex align-items-center p-4 rounded-3 bg-white" id='sign-container'>
         <form action="<?php echo ROOT_PATH.'/login' ?>" class="w-100 p-lg-4" method="post">
-            <h1 class="font-weight-bold">De retour parmis nous ?</h1>
+            <h1 class="font-weight-bold">De retour parmi nous ?</h1>
 
             <h5 class="my-4">Informations obligatoires *</h5>
             <div class="form-group my-2">
-                <label for='login'>Adresse email ou Identifiant *</label>
+                <label for='login'>Adresse email ou pseudo *</label>
                 <input type='text' name='login' id='login' class="form-control" placeholder="prenom-nom@email.com" required>
             </div>
             <div class="form-group my-2">
