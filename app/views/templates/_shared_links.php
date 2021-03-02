7cc31d1fd7c186571728f74984c94c7100d68fdc
antoine-tech <antoinehadrienlegullou@outlook.com>
2021-03-02 12:25:11 +0100

    adding link in footer and shred link partial to reset password

--- app/views/templates/_shared_links.php
+++ app/views/templates/_shared_links.php
@@ -1,7 +1,9 @@
 <ul class="w-100 d-flex flex-column list-unstyled mt-4" style='list-style-type:none'>
+    <li><small><a href="<?php echo ROOT_PATH."/" ?>">Accueil</a></small></li>
     <li><small><a href="<?php echo ROOT_PATH."/signin" ?>">Se connecter</a></small>
     </li>
     <li><small><a href="<?php echo ROOT_PATH."/register" ?>">S'inscrire</a></small>
     </li>
-    <li><small><a href="<?php echo ROOT_PATH."/" ?>">Accueil</a></small></li>
+    <li><small><a href="<?php echo ROOT_PATH."/users/password/reset" ?>">Mot de passe oublié</a></small>
+    </li>
 </ul>
