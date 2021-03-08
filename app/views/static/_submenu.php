0815e165953437b8ba99dc5256e895cc3a1ce464
antoine-tech <antoinehadrienlegullou@outlook.com>
2021-03-08 12:22:21 +0100

    reviewing link and icons submenu

--- app/views/static/_submenu.php
+++ app/views/static/_submenu.php
@@ -2,13 +2,13 @@
          <li class="nav-item text-white"><i class="lni lni-32 lni-windows"></i><a
                  href="<?php echo ROOT_PATH."/posts?platform=1" ?>" class="nav-link">Informatique</a></li>
          <li class="nav-item text-white"><i class="lni lni-32 lni-signal"></i><a
-                 href="<?php echo ROOT_PATH."/posts?platform=2" ?>" class="nav-link">Box/Internet</a></li>
+                 href="<?php echo ROOT_PATH."/posts?platform=3" ?>" class="nav-link">Box/Internet</a></li>
          <li class="nav-item text-white"><i class="lni lni-32 lni-game"></i><a
                  href="<?php echo ROOT_PATH."/posts?platform=5" ?>" class="nav-link">Console de jeux</a></li>
-         <li class="nav-item text-white"><a href="<?php echo ROOT_PATH."/posts?platform=6" ?>"
+         <li class="nav-item text-white"><i class="lni lni-32 lni-display"></i><a href="<?php echo ROOT_PATH."/posts?platform=6" ?>"
                  class="nav-link">TV/Multimédia</a></li>
          <li class="nav-item text-white"><i class="lni lni-32 lni-mobile"></i><a
                  href="<?php echo ROOT_PATH."/posts?platform=2" ?>" class="nav-link">Smartphone/Tablette</a></li>
-         <li class="nav-item text-white"><a href="<?php echo ROOT_PATH."/posts?platform=4" ?>"
+         <li class="nav-item text-white"><i class="lni lni-32 lni-home"></i><a href="<?php echo ROOT_PATH."/posts?platform=4" ?>"
                  class="nav-link">Electroménager</a></li>
      </ul>
